d699bb65c1cd04e5e036912917db6c4a77a51e8d
Mp-keyner <keyneroswaldo0@gmail.com>
2023-07-25 23:48:48 -0500

    25 de julio 11:48

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+let selectedButton = null;
+const aro = document.getElementById("nomm");
+const botom = document.getElementById("botom");
+const add = document.querySelector(".add");
+const title = document.querySelector(".title");
 
 function ko(x, y) {
-    console.log('keyner');
-    const element = document.getElementById(x);
-    element.style.border = `1px solid ${y}`;
+  const element = document.getElementById(x);
+
+  if (selectedButton !== null) {
+    selectedButton.style.border = "1px solid transparent";
   }
-    
+
+  element.style.border = `1px solid black`;
+  aro.style.background = `${y}`;
+  add.style.background = `${y}`;
+  title.style.color = `${y}`;
+  botom.style.background = `${y}`;
+  if (y == "#98926d") {
+    botom.style.color = "black";
+  } else {
+    botom.style.color = "white";
+  }
+  selectedButton = element;
+}
