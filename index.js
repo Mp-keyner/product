bef65f3196d232dc72d1cf65f0c69c7329e46d82
Keyner De la Hoz <107326379+Mp-keyner@users.noreply.github.com>
2023-07-25 21:01:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const boton1 = document.getElementById("boton1");
-const boton2 = document.getElementById("boton2");
-const boton3 = document.getElementById("boton3");
-const boton4 = document.getElementById("boton4");
-const bol = document.querySelectorAll(".fondo");
 
-boton1.onclick = function () {
-  console.log("boton1 clicked");
-  fondo.style.background = "red";
-};
-boton2.onclick = function () {
-  console.log("boton2 clicked");
-};
-boton3.onclick = function () {
-  console.log("boton3 clicked");
-};
-boton4.onclick = function () {
-  console.log("boton4 clicked");
-};
-bol.onclick = function () {
-  console.log("fondo clicked");
-};
+function ko(x, y) {
+    console.log('keyner');
+    const element = document.getElementById(x);
+    element.style.border = `1px solid ${y}`;
+  }
+    
